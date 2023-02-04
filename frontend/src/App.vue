69908d8020e82797479125fddfb9fe5c5235b376
Robert Lieblang <roberttheanimal01@gmail.com>
2023-02-04 09:01:37 -0600

    Continue frontend setup (not done)
    Initial frontend setup (in progress)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- add a side nav bar component later -->
+    <div class="container flex-grow-1">
+      <error />
+      <div class="mt-5">
+        <router-view />
+      </div>
+    </div>
+    <div>
+      <h1 class="bg-success">Schedule Optimizer</h1>
+      <p>Your schedule needs improvement!</p>
+    </div>
+    <footer class="bg-light p-3">
+      <p>this is some text in the footer</p>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <home-content />
+    </div>
+</template>
+
+<script>
+import HomeContent from '@/components/HomeContent.vue';
+
+export default {
+    name: "home-view", 
+    components: {
+        HomeContent,
+    },
+};
+</script>
+
+<style scoped>
+    <!-- .next-steps .fa-link {
+        margin-right: 5px;
+    } -->
+</style>
