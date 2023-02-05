ca8a16ed75e69aab1ee45d6e1e8fb028ec84d75f
Robert Lieblang <roberttheanimal01@gmail.com>
2023-02-05 17:29:06 -0600

    Minor changes to text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <main className="column">
-      <h1>Auth0 Login</h1>
+      <h1>Schedule App</h1>
       {error && <p>Authentication Error</p>}
       {!error && isLoading && <p>Loading...</p>}
       {!error && !isLoading && (
--- frontend/src/components/LoginButton.js
+++ frontend/src/components/LoginButton.js
@@ -6,7 +6,7 @@ const LoginButton = () => {
     return (
         !isAuthenticated && (
             <button onClick={() => loginWithRedirect()}>
-                Sign In
+                Sign In With Google
             </button>
         )
     )
