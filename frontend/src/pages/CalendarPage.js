9e48cfc012a2b4f32dd6bc736fd2d95139275445
kevinforbes2002 <kevin4bes2002@gmail.com>
2023-02-18 12:59:44 -0600

    Update CalendarPage.js

--- frontend/src/pages/CalendarPage.js
+++ frontend/src/pages/CalendarPage.js
@@ -11,7 +11,6 @@ import formatDate from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 export default class DemoApp extends React.Component {
 
