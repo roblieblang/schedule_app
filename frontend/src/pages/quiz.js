56d061cb10481808820fd5c911f08965a6b01d8c
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-02-26 19:25:47 -0600

    a

---
+++ frontend/src/pages/quiz.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './quiz.css';
+import  QuizData from '../data/QuizData';
+
+function quiz() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+
+
+
+export default quiz;
+
+
+
+ 
