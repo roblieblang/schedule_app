f08000bad610f6aea56c9f49246edd86e27c10d0
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-02-28 16:25:02 -0600

    s

---
+++ frontend/src/components/buttons/SurveySubmitButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+import Button from '@mui/material/Button';
+
+export default function SubmitSurvey() {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/dashboard',
+      },
+      authorizationParams: {
+        prompt: 'login',
+      },
+    });
+  };
+
+  return (
+    <Button variant="contained" onClick={handleLogin} type="button">
+      Submit
+    </Button>
+  );
+}
--- frontend/src/pages/MyChronotypePage.js
+++ frontend/src/pages/MyChronotypePage.js
@@ -3,17 +3,26 @@ import NavBar from '../components/navBar/MainNavBar';
 import { Box } from '@mui/system';
 import ChronoSummary from '../components/chronotype/ChronoSummary';
 import ChronotypeTimeline from '../components/chronotype/ChronotypeTimeline';
+import TakeSurvey from '../components/buttons/SurveyButton';
 
 const MyChronotypePage = () => {
    
     return (
+
         <>
+        
             <NavBar />
             <Box sx={{ml: 32, mt: -5, mr: 10}}>
                 <ChronoSummary/>
                 <ChronotypeTimeline/>
             </Box>
-        </>
+
+            <TakeSurvey></TakeSurvey>
+
+         </>
+            
+
+        
     )
 }
 
