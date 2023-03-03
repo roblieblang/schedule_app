ce8c2ebbe523ded50ec8533fe382fa2d5ce23c88
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-03-03 14:19:16 -0600

    h

--- frontend/src/components/buttons/SurveySubmitButton.js
+++ frontend/src/components/buttons/SurveySubmitButton.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-
+import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 
 export default function SubmitSurvey() {
-  const { loginWithRedirect } = useAuth0();
-
-  const handleLogin = async () => {
-    await loginWithRedirect({
-      appState: {
-        returnTo: '/dashboard',
-      },
-      authorizationParams: {
-        prompt: 'login',
-      },
-    });
-  };
-
+  
   return (
-    <Button variant="contained" onClick={handleLogin} type="button">
-      Submit
-    </Button>
+    <Link to= "./mychronotype">
+            <button>Sumbit Survey</button>
+        </Link>
   );
-}
+};
