f04247417779562fba7f576294982a08fcd5317a
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-03-03 14:19:16 -0600

    h

--- frontend/src/components/buttons/SurveyButton.js
+++ frontend/src/components/buttons/SurveyButton.js
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import Card from './components/survey/survey';
 
 export default function SurveyButton() {
+
+    
+
+    
     return (
         <Link to= './quiz'>
             <button>Take Survey</button>
