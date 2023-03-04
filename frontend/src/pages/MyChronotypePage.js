c2715cd6712a3640ab3e31e886be377b214fa206
Robert Lieblang <roberttheanimal01@gmail.com>
2023-03-04 14:09:46 -0600

    Add ChronotypeTimeline import

--- frontend/src/pages/MyChronotypePage.js
+++ frontend/src/pages/MyChronotypePage.js
@@ -4,6 +4,7 @@ import { Box } from '@mui/system';
 import ChronoSummary from '../components/chronotype/ChronoSummary';
 import SurveyButton from '../components/buttons/SurveyButton';
 import '../stylesheets/mychronotype/chronotype.css';
+import ChronotypeTimeline from './../components/chronotype/ChronotypeTimeline';
 
 const MyChronotypePage = () => {
 
