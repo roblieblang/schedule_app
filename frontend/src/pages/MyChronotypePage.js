5d89ab9dd5795ceff4038b9654962d80e171dc83
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-03-04 13:42:41 -0600

    commit

--- frontend/src/pages/MyChronotypePage.js
+++ frontend/src/pages/MyChronotypePage.js
@@ -2,32 +2,41 @@ import React from 'react'
 import NavBar from '../components/navBar/MainNavBar';
 import { Box } from '@mui/system';
 import ChronoSummary from '../components/chronotype/ChronoSummary';
-import ChronotypeTimeline from '../components/chronotype/ChronotypeTimeline';
 import SurveyButton from '../components/buttons/SurveyButton';
 import '../stylesheets/mychronotype/chronotype.css';
+import ChronotypeTimeline from '../components/chronotype/ChronotypeTimeline';
 
 const MyChronotypePage = () => {
-   
-    return (
 
+    return (
         <>
-        
+
+
             <NavBar />
             <Box sx={{ml: 32, mt: -5, mr: 10}}>
-                
-                <ChronoSummary/>
-                <ChronotypeTimeline/>
-                <div className ='buttonClass111'><SurveyButton > </SurveyButton></div>
-                <br></br>
-            </Box>
 
-            
+            <div className ='buttonClass111'> <SurveyButton type='button'> </SurveyButton></div> 
 
-         </>
+            <br></br>
+
             
 
         
+            
+                <ChronoSummary/>
+
+               
+                
+                <ChronotypeTimeline/>
+                 
+               
+            </Box>
+
+           
+        </>
     )
 }
 
 export default MyChronotypePage;
+
+
