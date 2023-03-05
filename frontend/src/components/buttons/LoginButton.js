4bb2d2770e620f42aad1e2b2005916c0002659b7
Robert Lieblang <roberttheanimal01@gmail.com>
2023-03-05 15:38:35 -0600

    Disable forced google permission screen
    Was interfering with repeat calls to google APIs

--- frontend/src/components/buttons/LoginButton.js
+++ frontend/src/components/buttons/LoginButton.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from '@mui/material/Button';
+import axios from "axios";
 
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
@@ -9,7 +10,7 @@ export default function LoginButton() {
     await loginWithRedirect({
       access_type: 'offline', 
       connection_scope: 'https://www.googleapis.com/auth/calendar.events.readonly', 
-      approval_prompt: 'force',
+      // approval_prompt: 'force',
       appState: {
         returnTo: `/dashboard`,
       },
