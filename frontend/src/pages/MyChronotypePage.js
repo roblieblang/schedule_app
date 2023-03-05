8d34d89306790bea7dd5584eec7660e87d758782
Robert Lieblang <roberttheanimal01@gmail.com>
2023-03-05 15:41:36 -0600

    Remove whitespace from MyChronotypePage.js

--- frontend/src/pages/MyChronotypePage.js
+++ frontend/src/pages/MyChronotypePage.js
@@ -7,32 +7,18 @@ import '../stylesheets/mychronotype/chronotype.css';
 import ChronotypeTimeline from './../components/chronotype/ChronotypeTimeline';
 
 const MyChronotypePage = () => {
-
     return (
         <>
-
-
             <NavBar />
             <Box sx={{ml: 32, mt: -5, mr: 10}}>
-
             <div className ='buttonClass111'> <SurveyButton type='button'> </SurveyButton></div> 
-
             <br></br>
-
-            
-
-        
-            
                 <ChronoSummary/>
                 <ChronotypeTimeline/>
-                 
-               
             </Box>
-
-           
         </>
-    )
-}
+    );
+};
 
 export default MyChronotypePage;
 
