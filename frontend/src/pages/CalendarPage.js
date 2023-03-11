fca7e45dd65af62ccbf232fcb5a992636c3ad08e
kevinforbes2002 <kevin4bes2002@gmail.com>
2023-03-10 20:44:41 -0600

    General Cleanup

--- frontend/src/pages/CalendarPage.js
+++ frontend/src/pages/CalendarPage.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import '../stylesheets/calendar/CalendarPage.css';
-import NavBar from '../components/navBar/MainNavBar';
-import { useAuth0 } from '@auth0/auth0-react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import GetGoogleCalendarId from '../components/calendar/getGoogleCalendarId';
 
 const CalendarPage = () => {
